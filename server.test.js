c48bcb19d2dd4fdf832d7fa4f3b8bc44b52e992d
Shaofeng Li <62363348+666saofeng@users.noreply.github.com>
2023-02-01 15:59:22 -0500

    Revert "fix some bugs and add unit testing"
    This reverts commit a24619a8002b652b013f0ce6a0f0115d0b8f3d9c.

--- server.test.js
+++
@@ -1,8 +0,0 @@
-const request = require('supertest')
-const server = require("./server.js")
-
-// test('should first', () => { expect(server.) })
-test('Should return a response code of 200',async()=>{
-    const response = await request("http://localhost:3000").get('/healthz');
-    expect(response.statusCode).toBe(200);
-})
