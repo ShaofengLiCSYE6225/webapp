2826f57d6ef62b6096d118bce452640e3527b5c1
shaofeng li <2604566769@qq.com>
2023-02-01 17:17:55 -0500

    close the npm test

--- __test__/test.js
+++ __test__/test.js
@@ -1,9 +1,10 @@
 const request = require('supertest')
-import {server} from '../server';
+
+import {server} from '../server.js';
 
 
 test('should return a respond code of 200', async() => { 
     const respond = await request("http://localhost:3000").get("/healthz")
     expect (respond.statusCode).toBe(200)
-    // server.close();
+    server.close();
  })
