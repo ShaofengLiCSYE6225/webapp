ea5d2bf6289480c7485c95b278799d4d1523bd17
Shaofeng Li <62363348+666saofeng@users.noreply.github.com>
2023-02-07 20:40:09 -0500

    Update test.js

--- __test__/test.js
+++ __test__/test.js
@@ -6,5 +6,6 @@ import {server} from '../server.js';
 test('should return a respond code of 200', async() => { 
     const respond = await request("http://localhost:3000").get("/healthz")
     expect (respond.statusCode).toBe(200)
+    
     server.close();
  })
