2583696d56a10b3d54c5d05d484e8315e49c2fe1
shaofeng li <2604566769@qq.com>
2023-04-12 22:47:30 -0400

    test health and v2

--- __test__/test.js
+++ __test__/test.js
@@ -4,7 +4,7 @@ import {server} from '../server.js';
 
 
 test('should return a respond code of 200', async() => { 
-    const respond = await request("http://localhost:3000").get("/healthz")
+    const respond = await request("http://localhost:3000").get("/health")
     expect (respond.statusCode).toBe(200)
     // await sequelize.sync()
     await server.close();
